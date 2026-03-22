9ee5d9298255e2b7891195c6e4c8da73f035ce26
aiswarya08-moravaneni <aiswaryamoravaneni2705@gmail.com>
2026-03-22 18:13:08 +0530

    Update dashboard.py

--- aqi-project/dashboard.py
+++ aqi-project/dashboard.py
@@ -47,10 +47,6 @@ df = df.drop(columns=["co", "so2", "o3"], errors="ignore")
 
 
 from sklearn.ensemble import RandomForestRegressor
-
-
-
-
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
