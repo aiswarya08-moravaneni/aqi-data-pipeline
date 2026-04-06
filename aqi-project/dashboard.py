f9690f43a2a370f4f5c36a45a5954ae67585c953
aiswarya08-moravaneni <aiswaryamoravaneni2705@gmail.com>
2026-04-06 10:47:41 +0530

    Refactor database connection to use secrets
    Updated database connection to use secrets management.

--- aqi-project/dashboard.py
+++ aqi-project/dashboard.py
@@ -22,10 +22,8 @@ socket.setdefaulttimeout(30)
 def get_connection():
 
     return psycopg2.connect(
-
-        "postgresql://postgres.oxcycpqjisgegrhewdov:AiS%23u2%29jkfty@aws-1-ap-south-1.pooler.supabase.com:6543/postgres?sslmode=require"
-
-    )
+    st.secrets["DB_URL"]
+)
 
 
 conn = get_connection()
